d0673da05ff71a942f3f8614e88f369a7b6dc597
Nayana Silvers <naymanjari@capncronch.local>
2022-03-09 18:32:53 -0800

    bouncing ball added

--- sketch.js
+++ sketch.js
@@ -1,12 +1,21 @@
-
 let oscSps = [];
+let bounceBalls = [];
 
 function setup() {
 	createCanvas (400, 400);
+
 	for (var i = 0; i < 12; i++) {
 		oscSps[i] = new oscSp(i);
 	}
+	
 
+
+}
+
+function mousePressed() {
+	let r = random(-5, 5);
+	let b = new bounceBall(mouseX, mouseY, r, r);
+	bounceBalls.push(b);
 }
 
 
@@ -17,6 +26,11 @@ function draw() {
 	for (var i = 0; i < oscSps.length; i++) {
 		oscSps[i].move();
 		oscSps[i].display();
+	}
+
+	for (var i = 0; i < bounceBalls.length; i++) {
+		bounceBalls[i].move();
+		bounceBalls[i].display();
 	}
 
 
@@ -31,63 +45,85 @@ class oscSp {
 		this.dx;
 		this.dy;
 		this.oscspeed = 0;
-		
-		print (whichSp);
 
 	}
 
 	move() {
 		if (this.whichSp == 0) {
-			this.oscx = 100;
+			this.oscx = 195;
 			this.oscy = 0;
 		} else if (this.whichSp == 1) {
-			this.oscx = (sqrt(3)/2)*100;
-			this.oscy = (1/2)*-100;
+			this.oscx = (sqrt(3)/2)*195;
+			this.oscy = (1/2)*-195;
 		} else if (this.whichSp == 2) {
-			this.oscx = (1/2)*100;
-			this.oscy = (sqrt(3)/2)*-100;
+			this.oscx = (1/2)*195;
+			this.oscy = (sqrt(3)/2)*-195;
 		} else if (this.whichSp == 3) {
 			this.oscx = 0;
-			this.oscy = -100;
+			this.oscy = -195;
 		} else if (this.whichSp == 4) {
-			this.oscx = (-1/2)*100;
-			this.oscy = (sqrt(3)/2)*-100;
+			this.oscx = (-1/2)*195;
+			this.oscy = (sqrt(3)/2)*-195;
 		} else if (this.whichSp == 5) {
-			this.oscx = (sqrt(3)/2)*-100;
-			this.oscy = (1/2)*-100;
+			this.oscx = (sqrt(3)/2)*-195;
+			this.oscy = (1/2)*-195;
 		} else if (this.whichSp == 6) {
-			this.oscx = -100;
+			this.oscx = -195;
 			this.oscy = 0;
 		} else if (this.whichSp == 7) {
-			this.oscx = (sqrt(3)/2)*-100;
-			this.oscy = (1/2)*100;
+			this.oscx = (sqrt(3)/2)*-195;
+			this.oscy = (1/2)*195;
 		} else if (this.whichSp == 8) {
-			this.oscx = (-1/2)*100;
-			this.oscy = (sqrt(3)/2)*100;
+			this.oscx = (-1/2)*195;
+			this.oscy = (sqrt(3)/2)*195;
 		} else if (this.whichSp == 9) {
 			this.oscx = 0;
-			this.oscy = 100;
+			this.oscy = 195;
 		} else if (this.whichSp == 10) {
-			this.oscx = (1/2)*100;
-			this.oscy = (sqrt(3)/2)*100;
+			this.oscx = (1/2)*195;
+			this.oscy = (sqrt(3)/2)*195;
 		} else if (this.whichSp == 11) {
-			this.oscx = (sqrt(3)/2)*100;
-			this.oscy = (1/2)*100;
+			this.oscx = (sqrt(3)/2)*195;
+			this.oscy = (1/2)*195;
 		}
-		this.oscspeed += 0.05;
+		this.oscspeed += 0.01;
 		this.dx = map(sin(this.oscspeed), -1, 1, 0, this.oscx);
 		this.dy = map(sin(this.oscspeed), -1, 1, 0, this.oscy);
 	}
 	
 
 	display() {
+		noStroke ();
 		ellipse (this.dx, this.dy, 10, 10);
 	}
 
 
 }
 
-//class explodies {
 
 
-//}
+class bounceBall {
+	constructor(x, y , xSpeed, ySpeed){
+		this.bounceX = x;
+		this.bounceY = y;
+		this.bounceSpeedX = xSpeed;
+		this.bounceSpeedY = ySpeed;
+	}
+
+	move() {
+		this.bounceX += this.bounceSpeedX;
+		if (this.bounceX < -200 || this.bounceX > 200) {
+			this.bounceSpeedX *= -1;
+		}
+
+		this.bounceY += this.bounceSpeedY;
+		if (this.bounceY < -200 || this.bounceY > 200) {
+			this.bounceSpeedY *= -1;
+		}
+	}
+
+	display() {
+		circle(this.bounceX, this.bounceY, 10);
+		//print(this.bounceX + ', ' + this.bounceY);
+	}
+}
